19752a70e30c9526a66f312c399c308e1249f3db
shivani Mourya <shivanimouryaa2@gmail.com>
2026-07-14 23:18:50 +0530

    added files

---
+++ backend/src/main/java/com/shivani/SkyReserve/SkyReserveApplication.java
@@ -0,0 +1,13 @@
+package com.shivani.SkyReserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyReserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyReserveApplication.class, args);
+	}
+
+}
